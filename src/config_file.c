5de21c609d40f2240f4be79b3ce788040fa3fd99
Carlos Martín Nieto <cmn@dwim.me>
2014-03-31 08:08:17 +0200

    config: split out the refresh step
    This will be used by the writing commands in a later step.

--- src/config_file.c
+++ src/config_file.c
@@ -249,11 +249,34 @@ static int config_open(git_config_backend *cfg, git_config_level_t level)
 	return res;
 }
 
+/* The meat of the refresh, as we want to use it in different places */
+static int config__refresh(git_config_backend *cfg)
+{
+	git_strmap *values;
+	diskfile_backend *b = (diskfile_backend *)cfg;
+	struct reader *reader = NULL;
+	int error = 0;
+
+	values = git_strmap_alloc();
+	GITERR_CHECK_ALLOC(values);
+
+	reader = git_array_get(b->readers, git_array_size(b->readers) - 1);
+
+	if ((error = config_parse(values, b, reader, b->level, 0)) < 0)
+		goto out;
+
+	values = git__swap(b->header.values, values);
+
+out:
+	free_vars(values);
+	git_buf_free(&reader->buffer);
+	return error;
+}
+
 static int config_refresh(git_config_backend *cfg)
 {
 	int error = 0, updated = 0, any_updated = 0;
 	diskfile_backend *b = (diskfile_backend *)cfg;
-	git_strmap *values;
 	struct reader *reader = NULL;
 	uint32_t i;
 
@@ -272,18 +295,7 @@ static int config_refresh(git_config_backend *cfg)
 	if (!any_updated)
 		return (error == GIT_ENOTFOUND) ? 0 : error;
 
-	values = git_strmap_alloc();
-	GITERR_CHECK_ALLOC(values);
-
-	if ((error = config_parse(values, b, reader, b->level, 0)) < 0)
-		goto out;
-
-	values = git__swap(b->header.values, values);
-
-out:
-	free_vars(values);
-	git_buf_free(&reader->buffer);
-	return error;
+	return config__refresh(cfg);
 }
 
 static void backend_free(git_config_backend *_backend)
